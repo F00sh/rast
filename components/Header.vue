dbaaf45d577d05bd791b2235690b04fb71d06db5
F00sh <fooshmoola@gmail.com>
2025-01-31 22:44:35 +0100

    header md setup

--- components/Header.vue
+++ components/Header.vue
@@ -7,9 +7,9 @@
     class="w-full h-[420px] md:h-[750px] object-cover"
   />
 </div>
-  <div class="absolute text-white md:items-center justify-center flex flex-col w-full h-96">
-   <h1 class="bg-emerald-400 text-7xl md:text-[200px] font-black pb-3 px-28 md:mt-64 mr-20 md:mx-auto">RAST</h1>
-   <h2 class="bg-emerald-400 text-2xl md:text-6xl font-bold pt-6 pb-7 px-20 md:right-0 md:ml-mt-10 md:mt-20">UREĐENJE ZELENIH POVRŠINA</h2>
+  <div class="absolute text-white md:items-center justify-center flex flex-col w-full h-96 md:mt-[230px]">
+   <h1 class="bg-emerald-400 text-7xl md:text-[200px] font-black md:px-5 md:mx-auto">RAST</h1>
+   <h2 class="bg-emerald-400 text-2xl md:text-5xl font-bold md:right-0 md:ml-mt-10 md:mt-10">UREĐENJE ZELENIH POVRŠINA</h2>
   </div>
 </div>
 </template>

fb2b306dd7feb4c6858e375564d5939315691aa1
F00sh <fooshmoola@gmail.com>
2025-01-31 11:12:12 +0100

    sds

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <img src="/img/header_img.png" alt="header" class="w-full h-96 object-cover">
+<div class="flex h-2/3 md:h-2/3">
+  <div class="w-full h-96 md:h-[700px] overflow-hidden">
+  <img
+    src="/img/header_img.png"
+    alt="header"
+    class="w-full h-full object-cover"
+  />
+</div>
+  <div class="absolute flex md:items-end md:justify-end w-full" >
+    <img src="/img/lovor_img.png" alt="lovor-višnja" class="object-cover">
   </div>
+</div>
 </template>

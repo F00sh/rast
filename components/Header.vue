b15117b8aa9c4984c17e1cc84791ac6b579744b9
F00sh <fooshmoola@gmail.com>
2025-01-31 09:49:17 +0100

    nez

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <img src="/img/header_img.jpg" alt="header" class="w-full h-96 object-cover">
+    <img src="/img/header_img.png" alt="header" class="w-full h-96 object-cover">
   </div>
 </template>

8e9c91a45a19235f69f567919e75ca9dfcec15e8
F00sh <fooshmoola@gmail.com>
2025-02-01 16:04:04 +0100

    malo puno sereš

--- components/About.vue
+++ components/About.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="flex flex-col h-screen w-full items-center justify-center text-white">
-    <img src="/img/about_img.jpg" alt="vrt" class="-z-10 object-cover md:px-40 lg:px-64">
-    <div class="absolute flex flex-col bg-emerald-500 flex-1 justify-center rounded-r-full">
-      <h1 class=" text-5xl font-bold">o nama</h1>
-      <h2 class="pl-20 text-2xl font-medium">(najbolji vrtlari)</h2>
-    </div>
-    <div class="absolute bg-teal-500 right-0 text-md rounded-l-full">
-      <h3 class="font-bold text-2xl text-left">mali naslov</h3>
-      <p>Lorem, ipsu m dolor sit amet consectetur adipisicing elit. Id sint illo at, repellat temporibus aperiam blanditiis aliquid aspernatur omnis officia voluptatum cupiditate nemo sunt soluta fugit, odio, itaque nihil recusandae.</p>
+  <div class="w-full h-full flex-1 flex-col items-center justify-start">
+    <div class="w-full h-screen flex items-start justify-center">
+      <img src="/img/about_img.jpg" alt="Header" 
+           class="w-full md:mt-10 lg:mt-20 md:w-9/12 lg:w-8/12 h-full md:h-3/4 shadow-lg object-cover" />
+      <div class="bg-opacity-70 absolute w-full h-full flex flex-col items-start justify-center">
+        <h1 class="bg-emerald-400 w-3/4 md:w-2/3 lg:w-1/3 text-end text-white font-black text-6xl md:text-7xl lg:text-9xl py-10 pr-10 md:py-12 md:pr-12 lg:py-14 lg:pr-14">
+          O NAMA
+        </h1>
+        <div class="w-full flex flex-col mt-10 md:mt-20 lg:mt-40">
+          <h2 class="bg-emerald-300 text-white font-medium text-2xl md:text-2xl lg:text-4xl w-3/4 md:w-2/3 lg:w-1/2 ml-auto py-10 md:py-12 lg:py-14 px-10 md:px-12 lg:px-14">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa sed nihil consequatur deserunt vel animi! Eius expedita soluta molestiae numquam necessitatibus quae, modi corrupti magnam a. Nisi iure necessitatibus laudantium!
+          </h2>
+        </div>
+        
+      </div>
     </div>
     
   </div>

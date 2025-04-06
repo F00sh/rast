a79e53f8e110da54ada14825c47069b1b6539ed6
F00sh <fooshmoola@gmail.com>
2025-04-06 21:47:13 +0200

    kontakt ikona

--- components/Header.vue
+++ components/Header.vue
@@ -8,7 +8,7 @@
     <div class="bg-[#25544A] w-full lg:w-1/2 h-screen flex flex-col items-center lg:items-start justify-end">
       <div class="absolute z-20 flex flex-col w-2/3 lg:w-1/4 top-0 mt-20 justify-center items-center lg:items-start">
         <img class="ml-5 lg:ml-0" src="/public/img/logo_new_img.svg" alt="">
-        <a href="Footer" class="z-10 text-center flex items-center justify-between border-4 border-[#478957] text-[#69BE5C] font-bold text-xl tracking-[0.7rem] mt-10 px-4 pl-7 py-4 uppercase shadow-xl rounded-r-full"><h1>kontakt</h1><img class="w-16 ml-4 drop-shadow-lg" src="/public/img/logo_round_light.svg" alt=""></a>
+        <a href="Footer" class="z-10 text-center flex items-center justify-between border-4 border-[#478957] text-[#69BE5C] font-bold text-xl tracking-[0.7rem] mt-10 px-4 pl-7 py-4 uppercase shadow-xl rounded-r-full"><h1>kontakt</h1><img class="desktop-only w-16 ml-4 drop-shadow-lg" src="/public/img/logo_round_light.svg" alt=""></a>
       </div>
       <img src="/public/img/header_new_image.png" alt="">
     </div>

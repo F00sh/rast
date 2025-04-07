5b550ee67db55fee1a760ca856727683793fcc81
F00sh <fooshmoola@gmail.com>
2025-04-07 14:23:54 +0200

    dasd

--- components/Services.vue
+++ components/Services.vue
@@ -4,7 +4,7 @@
         <div class="flex flex-col items-start justify-center lg:items-start lg:justify-start h-full bg-[#25544A]">
           <h1 class="bg-[#25544A] flex h-full font-montserrat text-white text-4xl lg:text-6xl font-black tracking-[0.8rem] lg:tracking-[1.5rem] uppercase ml-10 lg:ml-0 mt-20">usluge</h1>
           <div class="bg-gray-100 h-1 w-1/3 ml-10 lg:ml-0 mt-5"></div>
-          <p class="bg-[#25544A] text-white h-full w-full pt-10 pb-20 lg:pb-10 lg:pr-5 px-10 lg:px-0 text-md lg:text-lg tracking-[0.25rem] leading-[1.5rem] lg:leading-[2.2rem] font-light uppercase">{{ usluge_mali_1 }}</p>  
+          <p class="bg-[#25544A] text-white h-full w-full pt-10 pb-20 lg:pb-20 lg:pr-5 px-10 lg:px-0 text-md lg:text-lg tracking-[0.25rem] leading-[1.5rem] lg:leading-[2.2rem] font-light uppercase">{{ usluge_mali_1 }}</p>  
         </div>
         
     </div>
@@ -20,17 +20,19 @@
       </div>
 
       <div class="mobile-only w-full flex flex-col gap-1 items-center justify-center text-center uppercase py-1">
-        <h1 class="bg-[#D35E29] flex h-full lg:leading-[2.2rem] tracking-[0.2rem] text-white text-center items-center text-lg lg:text-2xl py-5 px-20">{{ usluge_podnaslov_1 }}</h1>
-        <h1 class="bg-[#D35E29] flex h-full lg:leading-[2.2rem] tracking-[0.2rem] text-white text-center items-center text-lg lg:text-2xl py-5 px-20">{{ usluge_podnaslov_2 }}</h1>
-        <h1 class="bg-[#D35E29] flex h-full lg:leading-[2.2rem] tracking-[0.2rem] text-white text-center items-center text-lg lg:text-2xl py-5 px-20">{{ usluge_podnaslov_3 }}</h1>
-        <img src="/public/img/berba.png" alt="">
-        <h1 class="bg-[#D35E29] flex h-full lg:leading-[2.2rem] tracking-[0.2rem] text-white text-center items-center text-lg lg:text-2xl py-5 px-30">{{ usluge_podnaslov_5 }}</h1>
-        <h1 class="bg-[#D35E29] flex h-full lg:leading-[2.2rem] tracking-[0.2rem] text-white text-center items-center text-lg lg:text-2xl py-5 px-28">{{ usluge_podnaslov_6 }}</h1>
-        <h1 class="bg-[#D35E29] flex h-full lg:leading-[2.2rem] tracking-[0.2rem] text-white text-center items-center text-lg lg:text-2xl py-5 px-20">{{ usluge_podnaslov_7 }}</h1> 
+        <h1 class="bg-[#D35E29] w-full h-full lg:leading-[2.2rem] tracking-[0.2rem] text-white text-center items-center text-lg lg:text-2xl py-5">{{usluge_podnaslov_1}}</h1>
+        <h1 class="bg-[#D35E29] w-full h-full lg:leading-[2.2rem] tracking-[0.2rem] text-white text-center items-center text-lg lg:text-2xl py-5">{{usluge_podnaslov_2}}</h1>
+        <h1 class="bg-[#D35E29] w-full h-full lg:leading-[2.2rem] tracking-[0.2rem] text-white text-center items-center text-lg lg:text-2xl py-5">{{usluge_podnaslov_3}}</h1>
+        <h1 class="bg-[#D35E29] w-full h-full lg:leading-[2.2rem] tracking-[0.2rem] text-white text-center items-center text-lg lg:text-2xl py-5 px-5">{{usluge_podnaslov_4}}</h1>
+        <h1 class="bg-[#D35E29] w-full h-full lg:leading-[2.2rem] tracking-[0.2rem] text-white text-center items-center text-lg lg:text-2xl py-5">{{usluge_podnaslov_5}}</h1>
+        <h1 class="bg-[#D35E29] w-full h-full lg:leading-[2.2rem] tracking-[0.2rem] text-white text-center items-center text-lg lg:text-2xl py-5">{{usluge_podnaslov_6}}</h1>
       </div>
-      <img class="lg:w-1/2 mb-1" src="/public/img/dubrava_8.png" alt="">
-      <div class="flex flex-col lg:flex-row lg:w-1/2 lg:rounded-4xl text-[#E8E8E8] mb-1">
-        <img class="lg:w-1/2" src="/public/img/ai_garden_2.png" alt="">
+      <img class="lg:w-1/2" src="/public/img/dubrava_8.png" alt="">
+      <div class="lg:w-1/2 flex items-start">
+        <img class="w-1/2 mb-1" src="/public/img/dubrava_3.png" alt="">
+        <img class="w-1/2 mb-1" src="/public/img/dubrava_7.png" alt="">        
+      </div>
+      <div class="flex flex-col lg:flex-row lg:w-1/2 text-[#E8E8E8] mb-1">
         <p class="h-full text-gray-100 text-md lg:text-xl tracking-[0.25rem] leading-[1.8rem] lg:leading-[2.2rem] font-light uppercase p-10 lg:p-20 my-1">{{ usluge_mali_2 }}</p>     
       </div>
   </div>

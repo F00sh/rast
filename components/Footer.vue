86ffecab669dcca0ccb86907035f1911776f69ac
F00sh <fooshmoola@gmail.com>
2025-04-12 13:09:38 +0200

    aa

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,9 +5,9 @@
         <NuxtLink to="/#Header" class="h-1/4 w-1/4 flex items-center justify-center text-[#E8E8E8]"><img class="h-full" src="/public/img/logo_round_light.svg" alt=""></NuxtLink>
     
       
-      <div class="font-montserrat w-1/2 flex flex-col items-center justify-center text-center text-[#E8E8E8] tracking-[0.2rem] font-light"> 
-        <h1 class="pb-1 text-lg lg:text-3xl text-[var(--green-light)]">+385 90 555 555</h1>
-        <h1 class="pt-1 text-xl lg:text-4xl text-[var(--orange-dark)]">info@rast.hr</h1>
+      <div class="font-montserrat w-1/2 flex flex-col items-center justify-center text-center text-[#E8E8E8] tracking-[0.2rem] font-light px-5"> 
+        <h1 class="pb-1 text-sm lg:text-3xl text-[var(--green-light)]">+385 90 555 555</h1>
+        <h1 class="pt-1 text-md lg:text-4xl text-[var(--orange-dark)]">info@rast.hr</h1>
       </div>
       <div class="h-1/4 w-1/4 flex items-center justify-center text-[#E8E8E8]">
         <img class="h-full" src="/public/img/logo_new_img.svg" alt="">
